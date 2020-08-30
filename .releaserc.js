e9e1a69ddcb5842a7763971e483770279310e387
grubyak <grubyak@users.noreply.github.com>
2020-08-30 22:07:24 +0200

    fix(ci): attach assets on release (#10)
    Co-authored-by: Artur Kudlacz <akudlacz@adobe.com>

--- .releaserc.js
+++ .releaserc.js
@@ -6,11 +6,11 @@ module.exports = {
       "changelogFile": "CHANGELOG.md",
     }],
     "@semantic-release/npm",
-    ["@semantic-release/git", {
+    ["@semantic-release/github", {
       "assets": ["package.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/github',
+    '@semantic-release/git'
   ],
   branch: 'master',
   branches: ['master']
