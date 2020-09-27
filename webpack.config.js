8015c2b04fa39c94acabdeae06c92332a5b0957a
Artur Kudlacz <akudlacz@adobe.com>
2020-09-27 23:18:16 +0200

    chore(webpack): adjust building mechanism

--- webpack.config.js
+++ webpack.config.js
@@ -14,38 +14,38 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isDev = process.env.NODE_ENV !== 'production';
-const mode = isDev ? 'development' : 'production';
-const devtool = isDev ? 'inline-source-map' : false;
+const isProduction = process.env.NODE_ENV === 'production';
+const mode = isProduction ? 'production' : 'development';
+const devtool = isProduction ? false : 'source-map';
+
+console.log('Building for:', mode);
 
 module.exports = {
-    entry: './src/types.ts',
-    mode,
-    devtool,
-    output: {
-        globalObject: `(function(){ try{ return typeof self !== 'undefined';}catch(err){return false;}})() ? self : this`,
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'aem-spa-page-model-manager.js',
-        library: 'aemSpaPageModelManager',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [
-            {
-                test: /.ts$/,
-                exclude: /(node_modules|dist)/,
-                use: {
-                    loader: 'ts-loader'
-                },
-                enforce: 'post'
-            }
-        ]
-    },
-    resolve: {
-        extensions: [ '.ts' ]
-    },
-    externals: [ nodeExternals() ],
-    plugins: [
-        new CleanWebpackPlugin()
+  entry: './src/types.ts',
+  mode,
+  devtool,
+  output: {
+    globalObject: `(function(){ try{ return typeof self !== 'undefined';}catch(err){return false;}})() ? self : this`,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'aem-spa-page-model-manager.js',
+    library: 'aemSpaPageModelManager',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        exclude: /(node_modules|dist)/,
+        use: {
+          loader: 'ts-loader'
+        },
+        enforce: 'post'
+      }
     ]
+  },
+  resolve: {
+    extensions: [ '.ts' ]
+  },
+  externals: [ nodeExternals() ],
+  plugins: [ new CleanWebpackPlugin() ]
 };
