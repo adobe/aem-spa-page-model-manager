9ede8595d3f39e3ec8b01f126f98ea0d00c07873
Sushant Bhasin <sbhasin@Sushants-MacBook-Pro.local>
2020-10-08 17:32:03 +0530

    fix: Add check to handle removed properties

--- src/ModelStore.ts
+++ src/ModelStore.ts
@@ -92,7 +92,14 @@ export class ModelStore {
             if (data && data[Constants.ITEMS_PROP]) {
                 const localData = clone(newData);
                 const items = data[Constants.ITEMS_PROP] || {};
-
+                const oldData = items[itemKey];
+                
+                for (const property in oldData) {
+                    if (!localData.value[property]) {
+                        localData.value[property] = "";
+                    }
+                }
+                
                 items[itemKey] = localData.value;
                 data[Constants.ITEMS_PROP] = items;
             }
